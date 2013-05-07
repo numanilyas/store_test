d63ac103fa8a3646ba4fb49f4fe49885579f2a11
Numan Ilyas <numan.ilyas@gmail.com>
2013-05-07 15:48:05 -0400

    initial store

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OnlineStore::Application.config.secret_token = 'b4d41050ac5d0896d3c873d02bb32d024a337b5a7680eabfc8aaa1e44e132f22fa8b76fe50c4cc59aed02910fe64a6650b90c9205a721308a8260db01752ca56'
